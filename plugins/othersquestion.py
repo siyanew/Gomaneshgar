7a2a45c8e6cd0c28604b00be6a9f0768367cd7d9
siyanew <siavashganji@gmail.com>
2017-03-10 10:07:43 +0330

    Fixed ٌWork flow of questions

--- plugins/othersquestion.py
+++ plugins/othersquestion.py
@@ -2,15 +2,15 @@ import asyncio
 import copy
 
 from aifunctions import make_guess
-from bot import current_state, history, delete_step
+from bot import current_state, history, delete_step, set_step
 from message import Message
 
 
 @asyncio.coroutine
 def run(message, matches, chat_id, step):
     if step ==0:
-        current_state['question'] = matches[1]
-        history.append(copy.deepcopy(current_state))
+        current_state['question'] = matches[1].strip()
+        set_step(plugin, message, 1)
 
     if step == 1:
         if message['text'] in ["بله", "خیر"]:
@@ -18,12 +18,12 @@ def run(message, matches, chat_id, step):
                 return Message(chat_id).set_text(make_guess())
 
         current_state['answer'] = message['text']
-
+        history.append(copy.deepcopy(current_state))
         delete_step(message)
 
 
 plugin = {
     "name": "otherquestion",
     "run": run,
-    "patterns": ["(.+):\n(.*؟)"]
+    "patterns": ["(.+):\n(.*)"]
 }
